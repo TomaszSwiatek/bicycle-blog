bac48366cb70c5537944adc35d04c6d6f9922925
Tomasz Swiatek <tomasswiatek@gmail.com>
2019-06-12 02:26:31 +0200

    created singup login welcome and navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,13 @@ import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 // import material modules from external file:
 import { MaterialModule } from "./material.module";
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SignupComponent, LoginComponent, NavbarComponent, WelcomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
