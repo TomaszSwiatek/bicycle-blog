3bb953e203a1ffacc1fd1b8326223dd13aa496a6
Tomasz Swiatek <tomasswiatek@gmail.com>
2019-06-17 11:11:52 +0200

    created admin panel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,13 @@
+import { AdminPanelComponent } from "./admin-panel/admin-panel.component";
 import { LoginComponent } from "./auth/login/login.component";
 import { SignupComponent } from "./auth/signup/signup.component";
-import { WelcomeComponent } from "./welcome/welcome.component";
-
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: "",
-    component: WelcomeComponent
+    path: "admin",
+    component: AdminPanelComponent
   },
   {
     path: "signup",
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,20 +9,20 @@ import { MaterialModule } from "./material.module";
 import { SignupComponent } from "./auth/signup/signup.component";
 import { LoginComponent } from "./auth/login/login.component";
 
-import { WelcomeComponent } from "./welcome/welcome.component";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { FormsModule } from "@angular/forms";
 import { SidenavListComponent } from "./navigation/sidenav-list/sidenav-list.component";
 import { HeaderComponent } from "./navigation/header/header.component";
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
     LoginComponent,
-    WelcomeComponent,
     SidenavListComponent,
-    HeaderComponent
+    HeaderComponent,
+    AdminPanelComponent
   ],
   imports: [
     BrowserModule,
